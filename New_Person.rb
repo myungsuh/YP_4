6dc628f345742d21bdf438f36ce2bd4b47e6b1d6
Myung Suh Son <myungsuhson@gmail.com>
2017-11-14 00:21:49 -0700

    Changed some names for the different modules referenced in module.rb also added input function to create some friendly user interactions

---
+++ New_Person.rb
@@ -0,0 +1,5 @@
+module New_Person
+  def speak_now
+    puts "Hello, how are you doing?"
+  end
+end
--- module.rb
+++ module.rb
@@ -1,19 +1,37 @@
 require_relative "Dog"
 require_relative "Jumping"
+require_relative "New_Person"
 
-module Animal
-  def make_sound
-    puts "Woof!"
+class Person
+  include New_Person
+end
+
+puts "You have now met a new person! Press enter to continue!"
+enter = gets.chomp
+
+Sam = Person.new
+Sam.speak_now
+
+class Cool_Dog
+  include Dog
+  prepend Jumping
+end
+
+def input
+  if input == ["yes", "Yes", "YES"]
+    puts "What would you like to name your dog?"
+  elsif input == ["no", "NO", "No"]
+    puts "I'm sorry to hear that!"
+    abort "Please come back next time!"
   end
 end
 
-class Thing
-  include Animal
-end
-
-puts "You have encountered a wild animal! Press enter to continue!"
-enter = gets.chomp
+puts "\nWould you like to create a new dog?"
+please = gets.chomp
+please.input
 
 
-shai = Thing.new
-shai.make_sound
+dog1 = Cool_Dog.new
+
+dog1.name = gets.chomp
+dog1.bark
